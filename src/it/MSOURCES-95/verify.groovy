6e715b18fecae00ebbf9704a37a5a0a77e06d690
Plamen Totev <plamen.iv.totev@gmail.com>
2018-06-10 21:42:00 +0300

    [MSOURCES-95] Source JAR is re-created even if sources are not changed
    If there is an empty source directory (such as
    generated-sources/annotations for example) the sources JAR will be
    re-created even if the sources have not changed. The reason is the
    default include pattern: "**/*". It does not include the directory
    itself and if the directory is empty there is no way for
    Plexus Archiver to know if the archive is up to date
    (no file or directory to compare with the last modification time
    of the archive),
    so it does the safe thing and re-creates the archive.
    
    Change the default includes pattern to "**/**". That will cause the
    empty directory itself (generated-sources/annotations in our example)
    to be considered when checking the last modification date so the
    archive will not be re-created if it is up to date. Please note that
    the change of the pattern will not change what is included in the
    archive -- the directory itself will not be included, only its content.
    
    This closes #2

---
+++ src/it/MSOURCES-95/verify.groovy
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+File buildLog = new File( basedir, 'build.log' )
+
+String sourcesJarFileName = 'empty-source-directory-1.0-SNAPSHOT-sources.jar'
+String testSourcesJarFileName = "empty-source-directory-1.0-SNAPSHOT-test-sources.jar"
+
+assert buildLog.exists()
+
+// Make sure the jars are created on the first build
+assert buildLog.text =~ /(?i) Building jar: .*${sourcesJarFileName}/
+assert buildLog.text =~ /(?i) Building jar: .*${testSourcesJarFileName}/
+
+// Make sure the jars are not re-created on subsequent builds
+assert buildLog.text =~ /(?i) Archive .*${sourcesJarFileName} is uptodate/
+assert buildLog.text =~ /(?i) Archive .*${testSourcesJarFileName} is uptodate/
